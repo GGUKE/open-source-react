b865356f89eb478deac014856383077f6d7579e6
GGUKE <pllbong2020@naver.com>
2024-11-28 16:26:40 +0900

    2024-11-28

--- open_source4/src/index.js
+++ open_source4/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import state from './state';
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<Provider store={state}>
-		<App />
-	</Provider>
+	<App />
 );
